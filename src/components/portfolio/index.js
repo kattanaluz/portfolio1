8d9b390a355339f2e98b706c5cc32b5edcd26d29
kattana <kattanaluz@gmail.com>
2022-02-20 22:43:13 +0000

    Black style

--- src/components/portfolio/index.js
+++ src/components/portfolio/index.js
@@ -16,19 +16,21 @@ function Portfolio() {
           </a>
           <div className={css.drinkAppText}>
             <h3>Drink recipes project</h3>
-            <p>HTML | CSS | JAVASCRIPT | REACT.JS | API</p>
-            <p>
+            <p className={css.portfolioParagraph}>
+              HTML | CSS | JAVASCRIPT | REACT.JS | API
+            </p>
+            <p className={css.portfolioParagraph}>
               The application itself is a single page app where the user can
               search for drink recipes. The application was made using React.js.
               It has a few components and it is using a free API TheCocktailDB
               to fetch the content needed.
             </p>
-            <p>
+            <p className={css.portfolioParagraph}>
               While developing this project, I could practice how to work with
               an API and async function. I also created components, used props
               and developed the user interface.
             </p>
-            <p>
+            <p className={css.portfolioParagraph}>
               You can check out the project page{" "}
               <a href="https://stoic-elion-211222.netlify.app/" target="_blank">
                 here
