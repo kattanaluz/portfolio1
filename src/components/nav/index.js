3def185a04a37254308a5eb3cb0aac346b06711c
kattana <kattanaluz@gmail.com>
2022-02-23 19:20:03 +0000

    responsive design finished

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -2,22 +2,24 @@ import css from "./index.module.css";
 import image from "./logo.png";
 function Nav() {
   return (
-    <nav className={css.navBar} id="top">
+    <>
       <div className={css.logoDiv}>
         <img className={css.logo} alt="logo" src={image}></img>
       </div>
-      <div className={css.navElements}>
-        <div>
-          <a href="#about">about</a>
+      <nav className={css.navBar} id="top">
+        <div className={css.navElements}>
+          <div>
+            <a href="#about">about</a>
+          </div>
+          <div>
+            <a href="#portfolio">portfolio</a>
+          </div>
+          <div>
+            <a href="#contact">contact</a>
+          </div>
         </div>
-        <div>
-          <a href="#portfolio">portfolio</a>
-        </div>
-        <div>
-          <a href="#contact">contact</a>
-        </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
 
