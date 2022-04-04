3901f3ca07f398aa65a09d964ed7a0792f8e7569
kattana <kattanaluz@gmail.com>
2022-04-04 18:01:57 +0100

    added job tracker to the portfolio

--- src/components/landDisplay/index.js
+++ src/components/landDisplay/index.js
@@ -9,14 +9,12 @@ function LandDisplay() {
         <div className={css.landMessageWrapper}>
           <div className={css.landMessage}>
             <h1>
-              <span className={css.hello}>Hi, my name is</span>
-              <span className={css.name}>Kattana Luz.</span>
+              <span className={css.hello}>Hi, I am</span>
+              <span className={css.name}>Kattana Luz,</span>
             </h1>
           </div>
           <h2 className={css.landDisplayH2}>
-            <span className={css.angleBracket}>&lt;</span> I am a former lawyer
-            who is studying to become a web developer
-            <span className={css.angleBracket}>/&gt;</span>
+            a former lawyer who is training to become a web developer
           </h2>
         </div>
       </div>
--- src/components/portfolio/index.js
+++ src/components/portfolio/index.js
@@ -1,4 +1,5 @@
 import css from "./index.module.css";
+import jobTrackerImage from "./job_tracker_app.png";
 import drinkAppImage from "./drink_app.png";
 import itCrowdAppImage from "./it_crowd_quiz_app.png";
 import noughtCrossesImage from "./noughts_and_crosses_app.png";
@@ -12,6 +13,25 @@ function Portfolio() {
         <SingleProject
           unitProjectDivClass={css.unitProjectDiv}
           aTagClass={css.imageLink}
+          href={"https://bossanova.netlify.app/"}
+          imgTagClass={css.projectImage}
+          src={jobTrackerImage}
+          alt={"Job Tracker website"}
+          projectTextClass={css.projectText}
+          h4Text={"Job Tracker"}
+          technologiesClass={css.technologies}
+          technologiesText={"REACT | TAILWIND | MONGODB | AUTH0"}
+          projectText={`The application enables the users to be authenticated, add new jobs that they want to keep tracking, edit, filter and delete existing jobs, search for useful resources to get a tech role. 
+          It was a group (6 people) final project of the School of Code Bootcamp.`}
+          projectLinksClass={css.projectLinks}
+          pageLink={"https://bossanova.netlify.app/"}
+          gitLink={
+            "https://github.com/SchoolOfCode/final-project-repo-bossa-nova"
+          }
+        />
+        <SingleProject
+          unitProjectDivClass={css.unitProjectDiv}
+          aTagClass={css.imageLink}
           href={"https://drink-recipes-finder.netlify.app/"}
           imgTagClass={css.projectImage}
           src={drinkAppImage}
@@ -19,10 +39,10 @@ function Portfolio() {
           projectTextClass={css.projectText}
           h4Text={"Drinks Recipes"}
           technologiesClass={css.technologies}
-          technologiesText={"HTML | CSS | JAVASCRIPT | REACT"}
-          projectText={`The application itself is a single-page application where the user can
-          search for drink recipes. The application was made using CSS, vanilla JavaScript,
-          and React.js. I also used a free API (TheCocktailDB) to fetch the content needed.`}
+          technologiesText={"CSS | JAVASCRIPT | REACT"}
+          projectText={`The project is website that enables the user
+          search for drink recipes. The application was made using CSS module
+          and React.js. It also uses a free API to fetch the content needed.`}
           projectLinksClass={css.projectLinks}
           pageLink={"https://drink-recipes-finder.netlify.app/"}
           gitLink={
@@ -39,8 +59,8 @@ function Portfolio() {
           projectTextClass={css.projectText}
           h4Text={"Noughts and Crosses"}
           technologiesClass={css.technologies}
-          technologiesText={"HTML | CSS | JAVASCRIPT | REACT"}
-          projectText={`The project is a single-page website where users can play the Noughts and Crosses game. 
+          technologiesText={"CSS | JAVASCRIPT | REACT"}
+          projectText={`Single-page website that enables the user to play Noughts and Crosses game. 
           The application was built using CSS, vanilla JavaScript, and React.js.`}
           projectLinksClass={css.projectLinks}
           pageLink={"https://noughts-and-crosses-game.netlify.app/"}
@@ -57,9 +77,9 @@ function Portfolio() {
           projectTextClass={css.projectText}
           h4Text={"IT Crowd Quiz"}
           technologiesClass={css.technologies}
-          technologiesText={"HTML | CSS | JAVASCRIPT"}
-          projectText={`The project is a single-page trivia quiz game.
-          I built this project in the first month of learning JavaScript at the School of Code Bootcamp.`}
+          technologiesText={"HTML | CSS | VANILLA JAVASCRIPT"}
+          projectText={`The project is a single-page trivia quiz game using vanilla JavaScript, CSS and HTML.
+          It was built in the first month of learning JavaScript at the School of Code Bootcamp.`}
           projectLinksClass={css.projectLinks}
           pageLink={"https://it-crowd-quiz.netlify.app/"}
           gitLink={"https://github.com/kattanaluz/IT-crowd-quiz"}
